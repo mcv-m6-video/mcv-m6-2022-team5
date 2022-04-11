2f82c607982f39e98466896763a3b46368eab087
GJdu <briandu8@gmail.com>
2022-04-11 20:01:13 +0200

    added metric learning stuff

---
+++ week5/load_utils.py
@@ -0,0 +1,94 @@
+import os
+import pathlib
+import cv2
+import xmltodict
+from VehicleDetection import VehicleDetection
+
+def readDetectionsXML(path):
+  #Generates detection dictionary where the frame number is the key and the values are the info of the corresponding detection/s
+  
+    with open(path,"r") as xml_obj:
+        #coverting the xml data to Python dictionary
+        gt = xmltodict.parse(xml_obj.read())
+        #closing the file
+        xml_obj.close()
+    
+
+    detections = {}
+    for track in gt['annotations']['track']:
+        if track['@label'] == 'car':
+            for deteccion in track['box']:
+                if deteccion['@frame'] not in detections:
+                    detections[deteccion['@frame']] = []
+                vh = VehicleDetection(int(deteccion['@frame']), int(track['@id']), 
+                                                        float(deteccion['@xtl']), float(deteccion['@ytl']), 0, 0, 
+                                                        1.0, float(deteccion['@xbr']), float(deteccion['@ybr']))
+                if deteccion['attribute']['@name'] == 'parked' and deteccion['attribute']['#text'] == 'false':
+                    vh.setParked(False)
+                else:
+                    vh.setParked(True)
+                detections[deteccion['@frame']].append(vh)
+
+    return detections
+
+
+def getNotParkedCars(detections):
+    notParked = {}
+    for frame, objs in detections.items():
+        obj_notParked = []
+        for ob in objs:
+            if not ob.parked:
+                obj_notParked.append(ob)
+        if len(obj_notParked) > 0:
+            notParked[frame] = obj_notParked
+    return notParked
+
+
+def readDetections(path):
+  #Generates detection dictionary where the frame number is the key and the values are the info of the corresponding detection/s
+  
+    with open(path) as f:
+        lines = f.readlines()
+
+    detections = {}
+    for line in lines:
+        data = line.split(',')
+        if data[0] in detections:
+            detections[data[0]].append(VehicleDetection(int(data[0]), int(data[1]), float(data[2]), float(data[3]), float(data[4]), float(data[5]), float(data[6])))
+        else:
+            detections[data[0]] = [VehicleDetection(int(data[0]), int(data[1]), float(data[2]), float(data[3]), float(data[4]), float(data[5]), float(data[6]))]
+
+    return detections
+
+def buildTrackCarsDataset():
+    for seq in ['S01', 'S03', 'S04']:
+
+    # get cam names from seq folder
+        gt_path = f'../datasets/aic19-track1-mtmc-train/train/{seq}'
+        dirlist = [ item for item in os.listdir(gt_path) if os.path.isdir(os.path.join(gt_path, item)) ]
+
+        for cam in dirlist:
+            gt_path = f'../datasets/aic19-track1-mtmc-train/train/{seq}/{cam}/gt/gt.txt'
+            detections_list = readDetections(gt_path)
+
+            prev = '0'
+            for frame_num in detections_list:
+                print(f'Processing Frame: {seq}_{cam}_{frame_num}')
+
+                detections = detections_list[str(frame_num)]
+
+                frame_path = f'../datasets/aic19-track1-mtmc-train/train/{seq}/{cam}/frames/vdo_{prev}.png'
+                prev = frame_num
+
+                img = cv2.imread(frame_path)
+
+                if img is not None:
+                    for detection in detections:
+                        id = detection.ID
+                        bbox = detection.getBBox()
+                        size = detection.areaOfRec()
+
+                        crop = img[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+                        pathlib.Path(f'aicity_cars_dataset/{id}/').mkdir(parents=True, exist_ok=True)
+                        cv2.imwrite(f'aicity_cars_dataset/{id}/{seq}_{cam}_{frame_num}.png', crop)
---
+++ week5/metric_learning_utils.py
@@ -0,0 +1,142 @@
+import numpy as np
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class TripletLoss(nn.Module):
+    """
+    Triplet loss
+    Takes embeddings of an anchor sample, a positive sample and a negative sample
+    """
+
+    def __init__(self, margin):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, anchor, positive, negative, size_average=True):
+        distance_positive = (anchor - positive).pow(2).sum(1)  # .pow(.5)
+        distance_negative = (anchor - negative).pow(2).sum(1)  # .pow(.5)
+        losses = F.relu(distance_positive - distance_negative + self.margin)
+        return losses.mean() if size_average else losses.sum()
+
+class TripletMIT_split(Dataset):
+
+    def __init__(self, mit_split_dataset, split, transform=None):
+        self.dataset = mit_split_dataset
+        self.n_samples = len(self.dataset)
+        self.train = split == 'train'
+        self.transform = transform
+
+        if self.train:
+            self.train_labels = self.dataset.targets
+            self.train_data = self.dataset.samples
+            self.labels_set = set(self.train_labels)
+            self.label_to_indices = {label: np.where(np.asarray(self.train_labels) == label)[0]
+                                     for label in self.labels_set}
+
+        else:
+            self.test_labels = self.dataset.targets
+            self.test_data = self.dataset.samples
+            # generate fixed triplets for testing
+            self.labels_set = set(self.test_labels)
+            self.label_to_indices = {label: np.where(np.asarray(self.test_labels)  == label)[0]
+                                     for label in self.labels_set}
+
+            random_state = np.random.RandomState(29)
+
+            triplets = [[i,
+                         random_state.choice(self.label_to_indices[self.test_labels[i]]),
+                         random_state.choice(self.label_to_indices[
+                                                 np.random.choice(
+                                                     list(self.labels_set - {self.test_labels[i]})
+                                                 )
+                                             ])
+                         ]
+                        for i in range(len(self.test_data))]
+            self.test_triplets = triplets
+
+    def __getitem__(self, index):
+        if self.train:
+            img1, label1 = self.train_data[index], self.train_labels[index]
+            positive_index = index
+            while positive_index == index:
+                positive_index = np.random.choice(self.label_to_indices[label1])
+            negative_label = np.random.choice(list(self.labels_set - {label1}))
+            negative_index = np.random.choice(self.label_to_indices[negative_label])
+            img2 = self.train_data[positive_index]
+            img3 = self.train_data[negative_index]
+        else:
+            img1 = self.test_data[self.test_triplets[index][0]]
+            img2 = self.test_data[self.test_triplets[index][1]]
+            img3 = self.test_data[self.test_triplets[index][2]]
+
+        img1 = Image.open(img1[0])
+        img2 = Image.open(img2[0])
+        img3 = Image.open(img3[0])
+
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            img3 = self.transform(img3)
+        return (img1, img2, img3), []
+
+    def __len__(self):
+        return self.n_samples # if you want to subsample for speed
+
+class TripletNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(TripletNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        output3 = self.embedding_net(x3)
+        return output1, output2, output3
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
+
+class EmbeddingNet_V3(nn.Module):
+    def __init__(self, backbone, model_id):
+        super(EmbeddingNet_V3, self).__init__()
+
+        basemodel = torch.hub.load('pytorch/vision:v0.10.0', backbone, pretrained=True)
+
+        self.model_id = model_id
+        self.base_resnet = torch.nn.Sequential(*(list(basemodel.children())[:-1]))
+
+        fc_layer = nn.Sequential(nn.Linear(2048, 256),
+                                 nn.PReLU(),
+                                 nn.Linear(256, 256),
+                                 nn.PReLU(),
+                                 nn.Linear(256, 2)
+                                 )
+        self.fc = fc_layer if "fc" in self.model_id else None
+
+    def forward(self, x):
+        output = self.base_resnet(x).squeeze()
+        if "fc" in self.model_id:
+            output = self.fc(output)
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+def extract_embeddings(dataloader, model, size):
+    with torch.no_grad():
+        model.eval()
+        embeddings = np.zeros((len(dataloader.dataset), size))
+        labels = np.zeros(len(dataloader.dataset))
+        k = 0
+        for images, target in dataloader:
+            images = images.to(device)
+            embeddings[k:k+len(images)] = model.get_embedding(images).data.cpu().numpy()
+            labels[k:k+len(images)] = target.numpy()
+            k += len(images)
+    return embeddings, labels
